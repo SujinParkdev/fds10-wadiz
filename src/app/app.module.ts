d651678ad396aaf0b058af93f612fd985b31f2a0
doogit <jaedoosong@gmail.com>
2018-07-25 21:45:27 +0900

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RoutingModule } from './/routing.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { JoinComponent } from './join/join.component';
+import { SettingComponent } from './setting/setting.component';
+import { DetailComponent } from './detail/detail.component';
+import { SearchComponent } from './search/search.component';
+import { FundingComponent } from './funding/funding.component';
+import { PaymentComponent } from './payment/payment.component';
+import { LikelistComponent } from './likelist/likelist.component';
+import { FundinglistComponent } from './fundinglist/fundinglist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    LoginComponent,
+    JoinComponent,
+    SettingComponent,
+    DetailComponent,
+    SearchComponent,
+    FundingComponent,
+    PaymentComponent,
+    LikelistComponent,
+    FundinglistComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/fundinglist/fundinglist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fundinglist',
+  templateUrl: './fundinglist.component.html',
+  styleUrls: ['./fundinglist.component.css']
+})
+export class FundinglistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
