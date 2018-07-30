1477d1de058e7ca6b0fd2de3ff9a5e7e831cbd62
doogit <jaedoosong@gmail.com>
2018-07-31 01:58:42 +0900

    update header animation, main category css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -33,6 +34,7 @@ import { FundinglistComponent } from './components/fundinglist/fundinglist.compo
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
