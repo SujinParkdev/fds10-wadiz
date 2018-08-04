80eeb0a439111bbd3e02e077dfc6601b74a126d7
zoonull <jkargo3@gmail.com>
2018-08-04 17:44:38 +0900

    File :: Modify join.component.ts
    회원가입 폼 작성 / 벌마 적용

--- src/app/components/join/join.component.ts
+++ src/app/components/join/join.component.ts
@@ -3,16 +3,34 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-join',
   template: `
-    <div class="join-container">join</div>
+    <div class="join-container content">
+        <h2>회원가입</h2>
+        <p>와디즈 펀딩을 위한 최소한의 정보를 받고있습니다.</p>
+        <div class="field">
+        <p class="control is-expanded has-icons-left has-icons-right">
+          <input class="input is-success" type="email" placeholder="Email" value="alex@smith.com">
+          <span class="icon is-small is-left">
+            <i class="fas fa-envelope"></i>
+          </span>
+          <span class="icon is-small is-right">
+            <i class="fas fa-check"></i>
+          </span>
+        </p>
+      </div>
+    </div>
+
   `,
   styles: [`
     .join-container {
       margin: 0 auto;
       width: 100%;
       max-width: 400px;
-      padding: 40px 16px 96px 16px;
-      background: lime;
-    }
+      padding: 16px 16px 32px;
+      background: grey;
+    },
+
+
+
   `]
 })
 export class JoinComponent implements OnInit {
