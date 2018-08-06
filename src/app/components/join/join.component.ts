b6ef6ada9e5577434b939190479cc695de6e5821
zoonull <jkargo3@gmail.com>
2018-08-06 15:30:35 +0900

    File :: Modify join.component & login.component
    join css 추가 / login ts 수정

--- src/app/components/join/join.component.ts
+++ src/app/components/join/join.component.ts
@@ -4,34 +4,44 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-join',
   template: `
     <div class="join-container content">
-        <h2>회원가입</h2>
-        <p>와디즈 펀딩을 위한 최소한의 정보를 받고있습니다.</p>
-        <div class="field">
-        <p class="control is-expanded has-icons-left has-icons-right">
-          <input class="input is-success" type="email" placeholder="Email" value="alex@smith.com">
-          <span class="icon is-small is-left">
-            <i class="fas fa-envelope"></i>
-          </span>
-          <span class="icon is-small is-right">
-            <i class="fas fa-check"></i>
-          </span>
-        </p>
+      <p class="join-large-text">회원가입</p>
+      <p class="join-medium-text">와디즈 펀딩을 위한 최소한의 정보를 받고있습니다.</p>
+      <!--이메일 섹션-->
+      <form name="frm_join" id="frm_join" method="post" class="wz form">
+      <div id="email-field" class="field check-email-field" >
+        <div class="wz input action">
+          <input id="userName" name="userName" type="email" class="input email large" placeholder="이메일 계정" readonly="true" required>
+          <button id="btnCheckEmail" class="wz button primary dense" type="button" disabled="true">인증하기</button>
+        </div>
       </div>
+      <!--이름 섹션-->
+      <div class="field">
+        <div class="wz input icon">
+          <input id="nickName" name="nickName" type="text" class="input text block large" maxlength="20" placeholder="이름" value="" readonly="true" required>
+          <i class="icon person-o"></i>
+        </div>
+      </div>
+      <!--비밀번호 섹션-->
+      <div class="field group">
+      <div class="field">
+        <div class="wz input icon">
+          <input id="pwd1" type="password" name="password" class="input large" placeholder="비밀번호 입력" readonly="true" required>
+          <i class="icon lock-o"></i>
+        </div>
+      </div>
+      <div class="field">
+        <div class="wz input icon">
+          <input id="pwd2" type="password" name="password2" class="input large" placeholder="비밀번호 확인" readonly="true" required>
+          <i class="icon lock-o"></i>
+        </div>
+          <em class="helper">영문, 숫자, 특수문자 (!@#$%^&*+=-)를 조합한 8자 이상</em>
+        </div>
+      </div>
+      <button type="submit" id="btnJoin" class="wz-btn primary block" disabled>완료</button>
+      </form>
     </div>
-
   `,
-  styles: [`
-    .join-container {
-      margin: 0 auto;
-      width: 100%;
-      max-width: 400px;
-      padding: 16px 16px 32px;
-      background: grey;
-    },
-
-
-
-  `]
+  styleUrls: [`./join.component.css`,`./join2.component.css`]
 })
 export class JoinComponent implements OnInit {
 
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,7 +3,44 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   template: `
-    <div class="login-container">login</div>
+    <div class="login-container">
+
+      <div class="user-sign-container">
+      <div class="wz container form-container form-login">
+        <h2 class="wz text display2">로그인</h2>
+        <form name="frm_login" id="frm_login" class="wz form">
+          <div class="field">
+            <div class="wz input">
+              <input type="email" id="userName" name="userName" class="input-text" placeholder="이메일 아이디">
+            </div>
+            <em id="emailError" class="error-text helper error">이메일 형식이 올바르지 않습니다.</em>
+          </div>
+          <div class="field">
+            <div class="wz input">
+              <input type="password" id="password" name="password" class="input-text"  maxlength="17"  placeholder="비밀번호(영문, 숫자, 특수문자 포함 8자 이상)">
+            </div>
+            <p id="loginError" class="error-text helper error">와디즈에 등록되지 않은 아이디거나, 아이디 또는 비밀번호가 회원정보와 일치하지 않습니다.</p>
+          </div>
+          <div class="recaptcha">
+            <p id="recaptchaMessage" class="error-text">잘못된 아이디 또는 비밀번호로 여러번 로그인 시도하였습니다. <br>계정 보안 조치를 위해 아래 사항을 체크하신 뒤 다시 진행하세요.</p>
+            <p id="recaptchaComplete" class="complete-text"><span>계정 보안 조치가 완료되었습니다!</span>협조해주셔서 감사합니다. 로그인을 다시 시도하세요</p>
+            <div class="g-recaptcha" data-sitekey="6Lcr70MUAAAAAHfmxpaHaXPDlgLIwiS6gRO5YCcB" data-size="600" id="recaptcha" data-callback="recaptchaComplete" style="display: none;"></div>
+          </div>
+          <div class="login-action wzui">
+            <label id="saveIdLabel" class="save">
+              <input id="saveUserId" type="checkbox" title="아이디 저장">
+              <span id="txtSaveId" class="wz text caption2">아이디 저장</span>
+            </label>
+          </div>
+          <button type="button" id="btnLogin" class="wz button primary block submit-button">로그인</button>
+        </form>
+        <div class="bottom-message">
+          <p>아직 와디즈 계정이 없나요?<a href="/web/waccount/wAccountRegistIntro" data-return-url>회원가입</a></p>
+        </div>
+      </div>
+     </div>
+
+    </div>
   `,
   styles: [`
     .login-container {
