a32979245948621c6d1db89ee6dcddba349433dd
doogit <jaedoosong@gmail.com>
2018-08-19 21:07:39 +0900

    update login

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
-import { Router } from '@angular/router';
 import { DetailCommentComponent } from './detail-comment/detail-comment.component';
 import { Router, ActivatedRoute } from '@angular/router';
 
